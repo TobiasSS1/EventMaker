1200fa15ebc742bf911ea4e0929e9a77f99f4f97
TobiasSS1 <tss3331@hotmail.com>
2017-01-30 13:07:22 +0100

    Singleton og mappe/klasse opstilling #6

---
+++ EventMaker/Model/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class Event
+    {
+
+        int Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        string Place { get; set; }
+        DateTime DateTime { get; set; }
+
+        public Event()
+        {
+            ToString();
+        }
+    }
+
+
+}
---
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMaker.Model
+{
+    class EventCatalogSingleton
+    {
+
+        private static EventCatalogSingleton instance;
+
+        private EventCatalogSingleton() { }
+
+        public static EventCatalogSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EventCatalogSingleton();
+                }
+                return instance;
+            }
+        }
+
+
+        
+
+
+        public void addMetode()
+        {
+
+        }
+    }
+}
