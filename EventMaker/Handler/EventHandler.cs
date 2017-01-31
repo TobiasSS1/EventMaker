196fc5b8e4efcea030ea196cf2bc67620d9c7b5d
TobiasSS1 <tss3331@hotmail.com>
2017-01-31 11:01:49 +0100

    Har tilføjet forbindelse mellem EventHandler og Eventviewmodel er ved at lave metode til at tilføje et nyt event

---
+++ EventMaker/Handler/EventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMaker.ViewModel;
+
+namespace EventMaker.Handler
+{
+    class EventHandler
+    {
+       public EventViewModel EVM { get; private set; }
+    
+
+        public EventHandler(EventViewModel evm)
+        {
+            this.EVM = evm;
+        }
+
+        public void CreateEvent()
+        {
+            EVM = new EventViewModel();
+        }
+    }
+}
--- EventMaker/ViewModel/EventViewModel.cs
+++ EventMaker/ViewModel/EventViewModel.cs
@@ -9,5 +9,19 @@ namespace EventMaker.ViewModel
     class EventViewModel
     {
         public Model.EventCatalogSingleton ECS { get; set; }
+        int Id { get; set; }
+        string Navn { get; set; }
+        string Description { get; set; }
+        string Place { get; set; }
+        DateTimeOffset Date { get; set; }
+        TimeSpan Time { get; set; }
+
+        public EventViewModel()
+        {
+            DateTime dt = System.DateTime.Now;
+            Date = new DateTimeOffset(dt.Year, dt.Month, dt.Day, 0, 0, 0, 0, new TimeSpan());
+            Time = new TimeSpan(dt.Hour, dt.Minute, dt.Second);
+        }
+
     }
 }
